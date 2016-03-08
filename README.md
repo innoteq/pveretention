c697264c0ed49a7ab8ab274eeee29f67b25fb55f
Michael Chinn <git@mechinn.com>
2016-03-08 00:40:02 -0500

    we can archive stuff now! both local and sftp archives, thanks for the idea reddit.com/user/brigzzy

--- README.md
+++ README.md
@@ -2,17 +2,31 @@
 
 ![Build Status](https://gitlab.com/mechinn/pveretention/badges/master/build.svg)
 
+To use ssh file transfer for off site backups install paramiko by executing: `pip install paramiko`
+
 ```
-usage: pveretention [-h] [-l LOCATION] [--vm VM] (-d DAYS | -c COUNT)
+usage: pveretention [-h] [-l LOCATION] [--vm VM] [-a ARCHIVE] [--ssh HOST]
+                    [--user USER] [--port PORT] [--key KEY]
+                    [--password PASSWORD] [--hostkey HOSTKEY]
+                    (-d DAYS | -c COUNT)
 
 Keep Proxmox Virtual Environment backups based on given retention.
 Either keep a number of backups for each VM or keep backups made within the last number of days.
 The user can also specify a limited set of VMs which this program should work on.
 
 optional arguments:
-  -h, --help            			show this help message and exit
-  -l LOCATION, --location location 	Manually set the location of the backups (default: /var/lib/vz/dump)
-  --vm VM               			Specify the VM(s) which should be included with this (default: All)
-  -d DAYS, --days DAYS  			The number of days backups should be kept (default: None)
-  -c COUNT, --count COUNT 			The number of backups that should be kept (default: None)
+	-h, --help            				show this help message and exit
+	-l LOCATION, --location LOCATION 	Manually set the location of the backups (default: /var/lib/vz/dump)
+	--vm VM               				Specify the VM(s) which should be included with this (default: ['All'])
+
+	-a ARCHIVE, --archive ARCHIVE 		Archive backups to location instead of deleting (default: None)
+		--ssh HOST             			Archive backups to ssh host at location defined with --archive (default: None)
+			--user USER           		Archive backups to ssh host with user (default: mechinn)
+			--port PORT           		Archive backups to ssh host at port (default: 22)
+			--key KEY             		Archive backups to ssh host with key auth (default: ~/.ssh/id_rsa)
+			--password PASSWORD   		Archive backups to ssh host with password auth (default: None)
+			--hostkey HOSTKEY     		Archive backups to ssh host validated with host key (default: None)
+
+	-d DAYS, --days DAYS  				The number of days backups should be kept (default: None)
+	-c COUNT, --count COUNT 			The number of backups that should be kept (default: None)
 ```
--- functional_test.sh
+++ functional_test.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 # simple stupid functional test
 
-mkdir test
-touch test/vzdump-lxc-1-2014_01_01-00_00_00.tar.gz
-touch test/vzdump-lxc-1-2014_01_01-00_00_00.log
-touch test/vzdump-lxc-1-2015_01_01-00_00_00.tar.gz
-touch test/vzdump-lxc-1-2015_01_01-00_00_00.log
-touch test/vzdump-lxc-1-2016_01_01-00_00_00.tar.gz
-touch test/vzdump-lxc-1-2016_01_01-00_00_00.log
-touch test/vzdump-lxc-2-2014_01_01-00_00_00.tar.gz
-touch test/vzdump-lxc-2-2014_01_01-00_00_00.log
-touch test/vzdump-lxc-2-2015_01_01-00_00_00.tar.gz
-touch test/vzdump-lxc-2-2015_01_01-00_00_00.log
-touch test/vzdump-lxc-2-2016_01_01-00_00_00.tar.gz
-touch test/vzdump-lxc-2-2016_01_01-00_00_00.log
+set -x
 
-if [ `ls test | wc -l` != 12 ]; then exit 1; fi
-python pveretention -l ./test --vm 1 -d 730
-if [ `ls test | wc -l` != 10 ]; then exit 2; fi
-python pveretention -l ./test --vm 2 -c 2
-if [ `ls test | wc -l` != 8 ]; then exit 3; fi
-python pveretention -l ./test -c 1
-if [ `ls test | wc -l` != 4 ]; then exit 4; fi
-python pveretention -l ./test -c 1
-if [ `ls test | wc -l` != 4 ]; then exit 5; fi
+#cleanup previous runs
+rm -rf backup
+rm -rf target
+
+mkdir backup
+touch backup/vzdump-lxc-1-2014_01_01-00_00_00.tar.gz
+touch backup/vzdump-lxc-1-2014_01_01-00_00_00.log
+touch backup/vzdump-lxc-1-2015_01_01-00_00_00.tar.gz
+touch backup/vzdump-lxc-1-2015_01_01-00_00_00.log
+touch backup/vzdump-lxc-1-2016_01_01-00_00_00.tar.gz
+touch backup/vzdump-lxc-1-2016_01_01-00_00_00.log
+touch backup/vzdump-lxc-2-2014_01_01-00_00_00.tar.gz
+touch backup/vzdump-lxc-2-2014_01_01-00_00_00.log
+touch backup/vzdump-lxc-2-2015_01_01-00_00_00.tar.gz
+touch backup/vzdump-lxc-2-2015_01_01-00_00_00.log
+touch backup/vzdump-lxc-2-2016_01_01-00_00_00.tar.gz
+touch backup/vzdump-lxc-2-2016_01_01-00_00_00.log
+mkdir target
+
+if [ `ls backup | wc -l` != 12 ] || [ `ls target | wc -l` != 0 ]; then exit 1; fi
+python pveretention -l ./backup --vm 1 -d 730 -a ./target
+if [ `ls backup | wc -l` != 10 ] || [ `ls target | wc -l` != 2 ]; then exit 2; fi
+python pveretention -l ./backup --vm 2 -c 2 -a ./target
+if [ `ls backup | wc -l` != 8 ] || [ `ls target | wc -l` != 4 ]; then exit 3; fi
+python pveretention -l ./backup -c 1 -a ./target
+if [ `ls backup | wc -l` != 4 ] || [ `ls target | wc -l` != 8 ]; then exit 4; fi
+python pveretention -l ./backup -c 1 -a ./target
+if [ `ls backup | wc -l` != 4 ] || [ `ls target | wc -l` != 8 ]; then exit 5; fi
+
+
